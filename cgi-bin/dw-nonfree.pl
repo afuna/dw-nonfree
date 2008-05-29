1fb442420ac17df61c5715d7e1b81f066464fafc
mark <mark@dreamwidth.org>
2008-05-29 16:31:21 -0700

    * fix account redirects.  FIXME: push this up to dw-free and generalize.

---
+++ cgi-bin/dw-nonfree.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+#
+# THIS IS NOT LICENSED FOR USE.
+#
+
+LJ::register_hook("journal_base", sub {
+    my ($u, $vhost) = @_;
+    return undef unless $LJ::ONLY_USER_VHOSTS;
+    return "#" unless $u;
+
+    # rule format:
+    #    accounttype => [$use_user_vhost_if_no_underscore, $domain_to_use_otherwise]
+
+    my $rules = {
+        'P' => [1, "users.$LJ::DOMAIN"],
+        'S' => [1, "users.$LJ::DOMAIN"],
+        'Y' => [0, "syndicated.$LJ::DOMAIN"],
+        'C' => [0, "community.$LJ::DOMAIN"],
+    };
+    my $rule = $rules->{$u->{journaltype}} || $rules->{'P'};
+
+    my $use_user_vhost =
+        $rule->[0] && $u->{user} !~ /^\_/ && $u->{user} !~ /\_$/;
+
+    if ($use_user_vhost) {
+        my $udom = $u->{user};
+        $udom =~ s/\_/-/g;
+        return "http://$udom.$LJ::DOMAIN";
+    } else {
+        return "http://$rule->[1]/$u->{user}";
+    }
+});
+
---
+++ cgi-bin/ljlib-local.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+#
+
+use lib "$ENV{LJHOME}/cgi-bin";
+require "dw-nonfree.pl";
+
+1;
