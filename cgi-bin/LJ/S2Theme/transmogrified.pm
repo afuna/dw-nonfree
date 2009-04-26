61d62c653cfadde8078d2a0190f782914bfd9979
afuna <afuna@dreamwidth.org>
2009-04-26 18:34:10 +0000

    http://bugs.dwscoalition.org/show_bug.cgi?id=993
    Cleanup wizard headings for Transmogrified.
    
    Patch by afuna.

--- cgi-bin/LJ/S2Theme/transmogrified.pm
+++ cgi-bin/LJ/S2Theme/transmogrified.pm
@@ -4,4 +4,72 @@ use base qw( LJ::S2Theme );
 sub cats { qw( featured ) }
 sub designer { "Yvonne" }
 
+sub page_props {
+    my $self = shift;
+    my @props = qw( color_page_title_background color_main_background );
+    return $self->_append_props( "page_props", @props );
+}
+
+sub header_props {
+    my $self = shift;
+    my @props = qw( color_header_background color_header_text color_header_hover_background color_header_hover );
+    return $self->_append_props( "header_props", @props );
+}
+
+sub module_props {
+    my $self = shift;
+    my @props = qw( color_module_title_background color_module_title_border );
+    return $self->_append_props( "module_props", @props );
+}
+
+sub entry_props {
+    my $self = shift;
+    my @props = qw(
+        color_entry_title_border
+        color_entry_title_background
+        color_entry_border_alt
+        color_entry_background_alt
+        color_entry_link_alt
+        color_entry_text_alt
+        color_entry_subject_alt
+        color_entry_subject_alt_border
+        color_entry_subject_alt_background
+    );
+    return $self->_append_props( "entry_props", @props );
+}
+
+sub comment_props {
+    my $self = shift;
+    my @props = qw( color_comments_form_border );
+    return $self->_append_props( "comment_props", @props );
+}
+
+sub footer_props {
+    my $self = shift;
+    my @props = qw( color_footer_background color_footer_text color_footer_link );
+    return $self->_append_props( "footer_props", @props );
+}
+
+sub archive_props {
+    my $self = shift;
+    my @props = qw( 
+        color_archivemonth_background
+        color_archivemonth_border
+        color_archivemonth_title_background
+        color_archivemonth_title_border
+        color_archivemonth_title
+    );
+    return $self->_append_props( "archive_props", @props );
+}
+
+sub navigation_props {
+    my $self = shift;
+    my @props = qw(
+        color_navigation_background
+        color_navigation_text
+        color_navigation_border
+    );
+    return $self->_append_props( "navigation_props", @props );
+}
+
 1;
