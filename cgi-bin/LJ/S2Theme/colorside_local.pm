0bedae9dcc1f3143257fb81de22512a480278f96
afuna <afuna@dreamwidth.org>
2009-08-22 16:42:43 +0000

    http://bugs.dwscoalition.org/show_bug.cgi?id=1661
    Four ColorSide themes by <user name='branchandroot'>: Atom, Fish, Fall Leaves, Wintergreen
    
    Patch by afuna.

--- cgi-bin/LJ/S2Theme/colorside_local.pm
+++ cgi-bin/LJ/S2Theme/colorside_local.pm
@@ -1,5 +1,21 @@
+package LJ::S2Theme::colorside::atom;
+use base qw( LJ::S2Theme::colorside );
+sub cats { qw() }
+
+package LJ::S2Theme::colorside::fallleaves;
+use base qw( LJ::S2Theme::colorside );
+sub cats { qw() }
+
+package LJ::S2Theme::colorside::fish;
+use base qw( LJ::S2Theme::colorside );
+sub cats { qw() }
+
 package LJ::S2Theme::colorside::nadeshiko;
 use base qw( LJ::S2Theme::colorside );
 sub cats { qw( featured ) }
+
+package LJ::S2Theme::colorside::wintergreen;
+use base qw( LJ::S2Theme::colorside );
+sub cats { qw() }
 
 1;
