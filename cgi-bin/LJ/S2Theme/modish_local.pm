cb2e461dd1a9138cf9a082a064d67bc76ad07f16
fu <fu@dreamwidth.org>
2009-09-02 17:41:11 +0000

    http://bugs.dwscoalition.org/show_bug.cgi?id=1661
    Modish themes: Lotus, Teal
    
    Patch by <user name='branchandroot'>, screenshots by <user name='piranha'>.

--- cgi-bin/LJ/S2Theme/modish_local.pm
+++ cgi-bin/LJ/S2Theme/modish_local.pm
@@ -1,5 +1,13 @@
+package LJ::S2Theme::modish::lotus;
+use base qw( LJ::S2Theme::modish );
+sub cats { qw() }
+
 package LJ::S2Theme::modish::scarlet;
 use base qw( LJ::S2Theme::modish );
 sub cats { qw( featured ) }
+
+package LJ::S2Theme::modish::teal;
+use base qw( LJ::S2Theme::modish );
+sub cats { qw() }
 
 1;
