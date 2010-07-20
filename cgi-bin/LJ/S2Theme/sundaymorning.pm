169f551ea2c10ed934f434dc71e2d883d05db6af
900degrees <900degrees@dreamwidth.org>
2010-07-20 14:46:05 -0500

    http://bugs.dwscoalition.org/show_bug.cgi?id=2793
    Whitespace cleanup and reordering for Sunday Morning.
    
    Patch by 900degrees.

--- cgi-bin/LJ/S2Theme/sundaymorning.pm
+++ cgi-bin/LJ/S2Theme/sundaymorning.pm
@@ -6,7 +6,6 @@ sub layout_prop { "layout_type" }
 
 sub designer { "regna" }
 
-
 package LJ::S2Theme::sundaymorning::greensquiggle;
 use base qw( LJ::S2Theme::sundaymorning );
 sub cats { qw() }
@@ -15,6 +14,10 @@ package LJ::S2Theme::sundaymorning::greenswirls;
 use base qw( LJ::S2Theme::sundaymorning );
 sub cats { qw() }
 
+package LJ::S2Theme::sundaymorning::lightondark;
+use base qw( LJ::S2Theme::sundaymorning );
+sub cats { qw() }
+sub designer { "cesy" }
 
 package LJ::S2Theme::sundaymorning::nnwm2009;
 use base qw( LJ::S2Theme::sundaymorning );
@@ -25,11 +28,6 @@ package LJ::S2Theme::sundaymorning::pinkswirls;
 use base qw( LJ::S2Theme::sundaymorning );
 sub cats { qw() }
 
-package LJ::S2Theme::sundaymorning::lightondark;
-use base qw( LJ::S2Theme::sundaymorning );
-sub cats { qw( ) }
-sub designer { "cesy" }
-
 package LJ::S2Theme::sundaymorning::purpleswirls;
 use base qw( LJ::S2Theme::sundaymorning );
 sub cats { qw() }
@@ -38,6 +36,7 @@ package LJ::S2Theme::sundaymorning::redcontrast;
 use base qw( LJ::S2Theme::sundaymorning );
 sub cats { qw() }
 sub designer { "ambrya" }
+
 package LJ::S2Theme::sundaymorning::redsquiggle;
 use base qw( LJ::S2Theme::sundaymorning );
 sub cats { qw() }
@@ -47,4 +46,3 @@ use base qw( LJ::S2Theme::sundaymorning );
 sub cats { qw() }
 
 1;
-
