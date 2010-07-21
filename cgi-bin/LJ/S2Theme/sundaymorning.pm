fbc59d47f93bc2ef73d3e5c3696aec60bd7978e6
900degrees <900degrees@dreamwidth.org>
2010-07-21 11:05:04 -0500

    http://bugs.dwscoalition.org/show_bug.cgi?id=2636
    Add support for two-columns-right layout to Sunday Morning.
    
    Patch by 900degrees.

--- cgi-bin/LJ/S2Theme/sundaymorning.pm
+++ cgi-bin/LJ/S2Theme/sundaymorning.pm
@@ -1,7 +1,7 @@
 package LJ::S2Theme::sundaymorning;
 use base qw( LJ::S2Theme );
 
-sub layouts { ( "1" => "one-column", "2l" => "two-columns-left" ) }
+sub layouts { ( "1" => "one-column", "2l" => "two-columns-left", "2r" => "two-columns-right" ) }
 sub layout_prop { "layout_type" }
 
 sub designer { "regna" }
