e7bf040ab416900a8be446d27d43be5406593760
900degrees <900degrees@dreamwidth.org>
2010-08-13 07:53:04 +0800

    http://bugs.dwscoalition.org/show_bug.cgi?id=	2793
    Just move this, to be consistent.
    
    Patch by 900degrees.

--- cgi-bin/LJ/S2Theme/transmogrified.pm
+++ cgi-bin/LJ/S2Theme/transmogrified.pm
@@ -1,10 +1,10 @@
 package LJ::S2Theme::transmogrified;
 use base qw( LJ::S2Theme );
 
-sub designer { "Yvonne" }
-
 sub layouts { ( "1" => "one-column", "2l" => "two-columns-left", "2r" => "two-columns-right" ) }
 sub layout_prop { "layout_type" }
+
+sub designer { "Yvonne" }
 
 sub page_props {
     my $self = shift;
